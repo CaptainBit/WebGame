5d70c0436f5dcdbf2fcff1b34e68d55fb7c420b5
Isaac Fiset <fisetisaac@hotmail.com>
2018-11-01 10:30:10 -0400

    add webService

---
+++ Backend-game_management/WebServices/src/java/Player/GenericResource.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Player;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.json.JSONException;
+
+import org.json.JSONObject;
+/**
+ * REST Web Service
+ *
+ * @author admin
+ */
+@Path("Player")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of Player.GenericResource
+     * @param userName
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("Connect")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@QueryParam("userName") String userName,@QueryParam("password") String password ) {
+        JSONObject json = new JSONObject();
+        try
+        {
+            json.append("userName", userName);
+            json.append("password", password);
+        }catch(JSONException e){
+            System.out.print(e.toString());
+        }
+        
+        return json.toString();
+    }
+
+}
