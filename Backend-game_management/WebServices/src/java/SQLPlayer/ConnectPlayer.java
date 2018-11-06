9f0b263f7ff2088e6529556886303804f000e4d4
Isaac Fiset <fisetisaac@hotmail.com>
2018-11-06 08:48:36 -0500

    Connection à la bd marche maintenant

--- Backend-game_management/WebServices/src/java/SQLPlayer/ConnectPlayer.java
+++ Backend-game_management/WebServices/src/java/SQLPlayer/ConnectPlayer.java
@@ -5,7 +5,11 @@
  */
 package SQLPlayer;
 
+
+
+
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,10 +22,25 @@ import org.json.JSONObject;
  */
 public class ConnectPlayer 
 {
+    public final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public final static String SERVERNAME= "jdbc:mysql://localhost:3306";
+    public final static String PORT = "80";
+    public final static String SCHEMA = "game_management";
+    public final static String PARAMETER = "?serverTimezone=UTC";
+    public final static String USERNAME = "root";
+    public final static String PASSWORD = "";
+    
     public JSONObject GetConnection(String userName, String password)
     {
-        SQLORDER so = new SQLORDER();
-        Connection con = so.SetConnection();
+         Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306" + "/" + SCHEMA +"?&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"; // a JDBC url
+            con = DriverManager.getConnection(url, USERNAME, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) 
+        {
+            System.out.print(e.toString());
+        }
         JSONObject jplayer = new JSONObject();
         try{
             PreparedStatement statement = con.prepareStatement("SELECT * FROM JOUEUR WHERE userName like ? and password like ? ;", 1005, 1008);     
