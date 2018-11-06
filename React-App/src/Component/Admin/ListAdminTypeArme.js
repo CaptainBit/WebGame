32a0838bbd5d5b499f13af7e418e1935a71f6204
William <william.lafontaine@infobec.ca>
2018-11-06 14:50:33 -0500

    Ajout section pour l'admin

---
+++ React-App/src/Component/Admin/ListAdminTypeArme.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class About extends Component {
+  
+  render() {
+    return (
+        <div>
+            Règle du jeu et condition d'utilisation
+        </div>
+    );
+  }
+}
+
+export default About;
