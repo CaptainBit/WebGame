93752a21ef9ae5bba4d22f069a56f10a7beae6fd
Isaac Fiset <fisetisaac@hotmail.com>
2018-11-20 11:48:54 -0500

    add timer (not functionnal)

--- Backend-game_management/WebServices/src/java/Services/GenericResource.java
+++ Backend-game_management/WebServices/src/java/Services/GenericResource.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Player;
+package Services;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
