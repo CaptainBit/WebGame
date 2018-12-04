21bbba19f1a0a96b209cbadc37d94bddcd9a6970
William <william.lafontaine@infobec.ca>
2018-12-04 09:56:13 -0500

    texte about

--- React-App/src/Component/Shared/About.js
+++ React-App/src/Component/Shared/About.js
@@ -18,11 +18,16 @@ class About extends Component {
 
     return (
       <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3">
+        <Typography variant="h4" component="h4">
         Règles du jeu et conditions d'utilisation
         </Typography>
-        <Typography component="p">
+        <br />
+        <Typography variant="h5" component="h5">
           Ce jeu a été développé par William Lafontaine et Isaac Fiset comme but éducatif et d'apprentissage avec le FrameWork React.
+          
+          Le but du jeu est simple, vous devez régner sur les autres joueurs en attaquant leurs territoires. Vous pouvez vous acheter
+          des soldats et équiper vos soldats d'une armure et d'une arme pour augmenter vos chances de gagner une bataille. Vous devez
+          vous servir judicieusement de vos ressources gagnés grâce à la possession d'un territoire. Bonne chance et amusez-vous bien!
         </Typography>
       </Paper>
     );
